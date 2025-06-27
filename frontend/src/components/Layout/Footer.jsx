94d36eb951a809ee520b506fd7a95995bb826c83
RitikLahari <2022ugcs123@nitjsr.ac.in>
2025-06-27 14:44:09 +0530

    a

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -10,10 +10,10 @@ function Footer() {
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
 <div>&copy; All Rights Reserved by Ritik.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/RitikLahari'} target='github'><FaGithub></FaGithub></Link>
+  <Link to={'https://leetcode.com/u/ritik_lahari01/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
+  <Link to={'https://in.linkedin.com/in/ritik-lahari-267213254'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  {/* <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link> */}
 </div>
       
     </footer>
